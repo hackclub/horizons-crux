9ff8bc695ef2275ee29371f8b6c70e71762c146b
Dhyan Tanna <79247409+D99-1@users.noreply.github.com>
2026-04-24 20:07:32 +1000

    Create qualifying.tsx

---
+++ qualifying.tsx
@@ -0,0 +1,56 @@
+import "../App.css";
+export default function Qualifying() {
+  return (
+    <div id="about-section">
+      <div id="about">
+        <h2>How to Qualify</h2>
+        <p>
+          To qualify for Horizons Crux, you need to spend <b>30 hours</b>{" "}
+          creating projects on{" "}
+          <a href="https://hackclub.com">Hack Club</a>.
+        </p>
+        <h3>Steps to Qualify:</h3>
+        <ol>
+          <li>
+            <b>Join Hack Club:</b> Visit{" "}
+            <a href="https://hackclub.com">hackclub.com</a> and create an
+            account
+          </li>
+          <li>
+            <b>Start Building:</b> Create projects and upload them to Hack Club
+          </li>
+          <li>
+            <b>Track Your Hours:</b> Your building time will be automatically
+            tracked
+          </li>
+          <li>
+            <b>Reach 30 Hours:</b> Once you accumulate 30 hours, you'll be
+            eligible to apply
+          </li>
+          <li>
+            <b>Apply:</b> Submit your application at{" "}
+            <a href="https://horizons.hackclub.com/crux">
+              horizons.hackclub.com/crux
+            </a>
+          </li>
+        </ol>
+        <h3>What Counts as Building Time?</h3>
+        <p>Any time spent creating projects on Hack Club counts toward your 30 hours:</p>
+        <ul>
+          <li>Coding projects (web, mobile, games, etc.)</li>
+          <li>Design work</li>
+          <li>Creative projects</li>
+          <li>Learning while building</li>
+        </ul>
+        <h3>Important Dates</h3>
+        <p>
+          <b>Qualifying Period:</b> Now through June 30, 2026
+          <br />
+          <b>Application Deadline:</b> June 30, 2026
+          <br />
+          <b>Event:</b> July 10-12, 2026 in Sydney
+        </p>
+      </div>
+    </div>
+  );
+}
