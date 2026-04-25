dce01e66c90f864111b673dd59728b53bc981225
Taran Mittal <taranmittal09@gmail.com>
2026-04-25 16:03:09 +1000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: '/your-repo-name',
+  assetPrefix: '/your-repo-name/',
 };
+
+module.exports = nextConfig;
+
 
 export default nextConfig;
