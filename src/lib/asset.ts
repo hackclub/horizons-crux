b4ea2d8f5b8aea21fda6ff0243314db3a5e97d33
Dekoder-py <kyle@codingcorner.dev>
2026-05-06 20:27:00 +1200

    create svlete project

--- src/lib/asset.ts
+++
@@ -1,7 +0,0 @@
-/** Prepend the deploy base path to any /public asset path. */
-export const asset = (path: string): string => {
-  const basePath = (process.env.NEXT_PUBLIC_BASE_PATH ?? "").replace(/\/+$/, "");
-  const normalizedPath = path.replace(/^\/+/, "");
-
-  return basePath ? `${basePath}/${normalizedPath}` : `/${normalizedPath}`;
-};
