9e1013f19a35d85656344db2c617347169e1b9a2
Taran Mittal <taranmittal09@gmail.com>
2026-05-13 15:23:59 +1000

    comit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,10 +18,10 @@ export default function Home() {
       <main>
         <Hero />
         <About />
-        <PastEvents />
         <Qualify />
         <Schedule />
         <Covered />
+        <PastEvents />
         <FAQ />
         <Sponsors />
       </main>
